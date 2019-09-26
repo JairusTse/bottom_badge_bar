2a4b47b0af9f2f8b0c2bdbffbe3a4ed6633b8006
JairusTse <13580533772@163.com>
2019-09-26 17:39:05 +0800

    支持显示未读消息的BottomNavigationBar

---
+++ test/bottom_badge_bar_test.dart
@@ -0,0 +1,9 @@
+import 'package:bottom_badge_bar/badge_bottom_navigation_bar.dart';
+import 'package:bottom_badge_bar/badge_bottom_navigation_bar_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+}
